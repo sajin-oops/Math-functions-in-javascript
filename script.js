80e04aa57ed4cd31b2bed3f7b01b24b508c46e08
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-09-30 14:09:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    
+    console.log(Math.round(5.8))
+    console.log(Math.floor(5.8))
+    console.log(Math.round(5.5))
+    console.log(Math.round(5.6))
+    console.log(Math.round(5.4))
+    console.log(Math.sqrt(285))
+    console.log(Math.abs(-5))   
+    console.log(Math.random()*6)
+    console.log(Math.random()*6)
+    console.log(Math.floor(Math.random()*6))+1
